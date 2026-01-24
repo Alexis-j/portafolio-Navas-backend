07ed7ac66e78d454a6462b8fe6cfeb06a8263f55
alexis jimenez castillo <alexis.jcastillo@outlook.com>
2026-01-24 21:15:00 +0100

    preparando deploy

--- config/db.js
+++ config/db.js
@@ -1,16 +1,10 @@
-import dotenv from 'dotenv';
 import pkg from 'pg';
-
-dotenv.config();
-
 const { Pool } = pkg;
 
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
+  connectionString: process.env.DATABASE_PUBLIC_URL, // Railway ya te da esto
+  ssl: { rejectUnauthorized: false }, // necesario para Railway
 });
 
 export default pool;
+
