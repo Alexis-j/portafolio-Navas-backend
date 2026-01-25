91b3d477e47c063c2bdaf02f86e5498e38cb8c41
alexis jimenez castillo <alexis.jcastillo@outlook.com>
2026-01-25 13:48:24 +0100

    cambios en la db

--- config/db.js
+++ config/db.js
@@ -2,9 +2,8 @@ import pkg from 'pg';
 const { Pool } = pkg;
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_PUBLIC_URL, // Railway ya te da esto
-  ssl: { rejectUnauthorized: false }, // necesario para Railway
+  connectionString: process.env.DATABASE_PUBLIC_URL,
+  ssl: { rejectUnauthorized: false }, 
 });
 
 export default pool;
-
