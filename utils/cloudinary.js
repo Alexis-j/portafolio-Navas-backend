604f3e6d03044c88d9e42b8e6156e2ef16933fc2
alexis jimenez castillo <alexis.jcastillo@outlook.com>
2026-01-27 20:18:46 +0100

    camcbios en cloudynary

--- utils/cloudinary.js
+++ utils/cloudinary.js
@@ -1,9 +1,8 @@
-import { v2 as cloudinary } from "cloudinary";
+export const cloudinary = require("cloudinary").v2;
 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
+  secure: true,
 });
-
-export { cloudinary };
