d443e313fe8536b098df1830de34fff73d0f5521
alexis jimenez castillo <alexis.jcastillo@outlook.com>
2026-01-28 09:35:16 +0100

    arreglando el mailer

--- utils/mailer.js
+++ utils/mailer.js
@@ -3,6 +3,8 @@ import nodemailer from "nodemailer";
 export const createTransporter = () => {
   return nodemailer.createTransport({
     service: "gmail",
+    port: 465,
+    secure: true, // 🔥 importante
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
